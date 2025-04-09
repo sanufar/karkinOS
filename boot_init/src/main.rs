130eaa0fa66206d32873f34f1f1d028f560f2143
Farhan <141865586+sanufar@users.noreply.github.com>
2025-04-09 05:41:44 -0400

    refactor: Moved boot_init disk reading to boot_utils/

--- boot_init/src/main.rs
+++ boot_init/src/main.rs
@@ -5,9 +5,7 @@ use core::arch::global_asm;
 use core::arch::asm;
 use core::panic::PanicInfo;
 
-use disk::DiskReader;
-
-mod disk;
+use boot_utils::{disk::*, print::*};
 
 const BOOTLOADER_LBA: u64 = 2048;
 const BOOTLOADER_SECTOR_SIZE: u16 = 64;
@@ -32,49 +30,15 @@ pub extern "C" fn main() -> ! {
     let offset_target = bootloader_start as u16;
     let mut disk = DiskReader::new(offset_target, BOOTLOADER_LBA);
 
-    disk.read_sectors(BOOTLOADER_SECTOR_SIZE);
+    if !disk.read_sectors(BOOTLOADER_SECTOR_SIZE) {
+        print("[!] Failed to load bootloader.\r\n\0");
+        loop {};  // Halt on error
+    }
 
     jump(bootloader_start);
 
     loop {};
     
-}
-
-
-fn print(message: &str) {
-
-    let offset: u16 = message.as_ptr() as u16;
-
-    unsafe {
-        asm!(
-                "mov si, {0:x}",
-            
-            // Label 'print_loop':
-            "2:",
-                // Load a byte from DS:SI into AL, increment SI
-                "lodsb",
-                // Check if AL is zero (end of string)
-                "test al, al",
-                // If zero, jump to 'done'
-                "jz 3f",
-
-                // Set up BIOS Teletype (AH=0x0E) to print AL
-                "mov ah, 0x0E",
-                "mov bh, 0",
-                "int 0x10",
-
-                // Jump back to 'print_loop'
-                "jmp 2b",
-
-            // Label 'done':
-            "3:",
-
-            // Pass the offset as input
-            in(reg) offset,
-            
-            clobber_abi("C"),
-        )
-}
 }
 
 fn clear() {
@@ -87,12 +51,6 @@ fn jump(address: *const u16) {
     unsafe {
         asm!("jmp {0:x}", in(reg) address as u16);
     }
-}
-
-#[no_mangle]
-pub extern "C" fn bootload_error() -> ! {
-    print("[!] Failed to load bootloader.");
-    loop {};
 }
 
 #[panic_handler]
