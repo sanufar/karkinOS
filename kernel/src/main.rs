b71ed8966c776848e460395cda583463a9e76a3d
Farhan <141865586+sanufar@users.noreply.github.com>
2025-04-10 12:31:10 -0400

    refactor: replaced bootloader with crate

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,12 +2,21 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-
-pub extern "C" fn _start() -> ! {
-    loop {};
-}
+use bootloader_api::BootInfo;
 
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    loop {};
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+bootloader_api::entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    if let Some(framebuffer) = boot_info.framebuffer.as_ref() {
+        let info = framebuffer.info();
+        let buffer = framebuffer.buffer();
+    }
+    loop {}
+}
+
+
