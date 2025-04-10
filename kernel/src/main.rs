1c1fc7af83196ca31481086372e2ee06a4db7a68
Farhan <141865586+sanufar@users.noreply.github.com>
2025-04-10 17:04:23 -0400

    feat: Added logging, enabled logging to serial stdio

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,20 +3,36 @@
 
 use core::panic::PanicInfo;
 use bootloader_api::BootInfo;
+use bootloader_api::info::FrameBufferInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+use conquer_once::spin::OnceCell;
+use bootloader_x86_64_common::logger::LockedLogger;
+
+pub(crate) static LOGGER: OnceCell<LockedLogger> = OnceCell::uninit();
+
+pub(crate) fn init_logger(buffer: &'static mut [u8], info: FrameBufferInfo) {
+    let logger = LOGGER.get_or_init(move || LockedLogger::new(buffer, info, true, true));
+    log::set_logger(logger).expect("Logger already set");
+    log::set_max_level(log::LevelFilter::Trace);
+    log::info!("Hello, Kernel Mode!");
 }
 
 bootloader_api::entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
-    if let Some(framebuffer) = boot_info.framebuffer.as_ref() {
-        let info = framebuffer.info();
-        let buffer = framebuffer.buffer();
-    }
+
+    let frame_buffer_optional = &mut boot_info.framebuffer;
+    let frame_buffer_option = frame_buffer_optional.as_mut();
+    let frame_buffer_struct = frame_buffer_option.unwrap();
+    let frame_buffer_info = frame_buffer_struct.info().clone();
+    let raw_frame_buffer = frame_buffer_struct.buffer_mut();
+    init_logger(raw_frame_buffer, frame_buffer_info);
+
     loop {}
 }
 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
 
