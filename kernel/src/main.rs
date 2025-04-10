93bcb304b9dacd19f00a685c42e8bf7f68acdd6c
Farhan <141865586+sanufar@users.noreply.github.com>
2025-04-09 21:43:39 -0400

    Revert "chore: plumbing"
    This reverts commit 1e0adce9050b8a16e969062802a43aa2aef70cd4.

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,13 +1,3 @@
-#![no_std]
-#![no_main]
-
-use core::panic::PanicInfo;
-
-pub extern "C" fn _start() -> ! {
-    loop {};
-}
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    loop {}
+fn main() {
+    println!("Hello, world!");
 }
