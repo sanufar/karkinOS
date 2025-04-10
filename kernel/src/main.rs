1e0adce9050b8a16e969062802a43aa2aef70cd4
Farhan <141865586+sanufar@users.noreply.github.com>
2025-04-09 20:04:35 -0400

    chore: plumbing

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+pub extern "C" fn _start() -> ! {
+    loop {};
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    loop {}
 }
