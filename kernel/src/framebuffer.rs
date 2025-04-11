6cd52f590bf69f613dda073158e6500035401990
Farhan <141865586+sanufar@users.noreply.github.com>
2025-04-10 23:47:00 -0400

    feat: Added embedded_graphics support

---
+++ kernel/src/framebuffer.rs
@@ -0,0 +1,105 @@
+use bootloader_api::info::{FrameBuffer, PixelFormat};
+use embedded_graphics::{
+    Pixel,
+    draw_target::DrawTarget,
+    geometry::{OriginDimensions, Size},
+    pixelcolor::{Rgb888, RgbColor},
+};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Color {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+}
+
+pub fn set_pixel(framebuffer: &mut FrameBuffer, position: Position, color: Color) {
+    let info = framebuffer.info();
+
+    let byte_offset = {
+        let y_offset = position.y * info.stride;
+        let pixel_offset = y_offset + position.x;
+
+        pixel_offset * info.bytes_per_pixel
+    };
+
+    let pixel_buffer = &mut framebuffer.buffer_mut()[byte_offset..];
+
+    match info.pixel_format {
+        PixelFormat::Rgb => {
+            pixel_buffer[0] = color.red;
+            pixel_buffer[1] = color.blue;
+            pixel_buffer[2] = color.green;
+        },
+        PixelFormat::Bgr => {
+            pixel_buffer[0] = color.blue;
+            pixel_buffer[1] = color.green;
+            pixel_buffer[2] = color.red;
+        },
+        PixelFormat::U8 => {
+            let gray = (color.red + color.green + color.blue)/3;
+            pixel_buffer[0] = gray;
+        },
+        other => panic!("Unknown pixel format: {other:?}"),
+    }
+}
+
+pub(crate) struct FramebufferDisplay<'f> {
+    framebuffer: &'f mut FrameBuffer
+}
+
+impl<'f> FramebufferDisplay<'f> {
+    pub fn new(framebuffer: &mut FrameBuffer) -> FramebufferDisplay {
+        FramebufferDisplay { framebuffer }
+    }
+
+    fn draw_pixel(&mut self, Pixel(coordinates, color): Pixel<Rgb888>) {
+        let (width, height) = {
+            let info = self.framebuffer.info();
+            (info.width, info.height)
+        };
+
+        let (x, y) = {
+            let c: (i32, i32) = coordinates.into();
+            (c.0 as usize, c.1 as usize)
+        };
+
+        if (0..width).contains(&x) && (0..height).contains(&y) {
+            let color = Color { red: color.r(), green: color.g(), blue: color.b() };
+
+            set_pixel(self.framebuffer, Position { x, y }, color);
+        }
+
+    }
+}
+
+impl<'f> DrawTarget for FramebufferDisplay<'f> {
+    type Color = Rgb888;
+
+    type Error = core::convert::Infallible;
+
+    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item = Pixel<Self::Color>> {
+        for pixel in pixels.into_iter() {
+            self.draw_pixel(pixel);
+        }
+
+        Ok(())
+    }
+}
+
+impl<'f> OriginDimensions for FramebufferDisplay<'f> {
+    fn size(&self) -> Size {
+        let info = self.framebuffer.info();
+
+        Size::new(info.width as u32, info.height as u32)
+    }
+}
+
