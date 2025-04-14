1f7c7c3e066e69756e1f76052219ea149de032ae
Farhan <141865586+sanufar@users.noreply.github.com>
2025-04-14 04:10:09 -0400

    refactor: Moved functionality to lib.rs

--- kernel/src/logger.rs
+++ kernel/src/logger.rs
@@ -4,7 +4,7 @@ use conquer_once::spin::OnceCell;
 
 pub(crate) static LOGGER: OnceCell<LockedLogger> = OnceCell::uninit();
 
-pub(crate) fn init(buffer: &'static mut [u8], info: FrameBufferInfo) {
+pub fn init(buffer: &'static mut [u8], info: FrameBufferInfo) {
     let logger = LOGGER.get_or_init(move || LockedLogger::new(buffer, info, true, true));
     log::set_logger(logger).expect("Logger already set");
     log::set_max_level(log::LevelFilter::Trace);
