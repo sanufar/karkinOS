c80a38f901bb4941e1a9453c4eeaa1e29344641b
Farhan <141865586+sanufar@users.noreply.github.com>
2025-04-14 04:05:32 -0400

    feat: Added a custom test flag (kerntest)
    Conditional cfg "kerntest" in order to avoid cargo test incompatibility with bootloader_api
    - proc macro to wrap would be too intensive
    
    TODO: Work on logger impl

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -4,8 +4,7 @@
 use bootloader_api::BootInfo;
 use core::panic::PanicInfo;
 
-mod framebuffer;
-mod logger;
+use kernel::*;
 
 bootloader_api::entry_point!(kernel_main);
 
@@ -17,10 +16,24 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     let raw_frame_buffer = frame_buffer_struct.buffer_mut();
     logger::init(raw_frame_buffer, frame_buffer_info);
 
+    custard();
+
     loop {}
+}
+
+fn custard() {
+    #[cfg(feature = "kerntest")]
+    {
+        log::info!("Running test...");
+        assert_eq!(1,1);
+        log::info!("[ok]")
+    }
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    log::error!("{}", _info);
     loop {}
 }
+
+
