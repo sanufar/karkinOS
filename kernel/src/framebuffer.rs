0f59829897e1912b776bfff16258f541a7a53102
Farhan <141865586+sanufar@users.noreply.github.com>
2025-04-28 09:19:19 -0400

    refactor: removed embedded_graphics, working on impl font rendering

--- kernel/src/framebuffer.rs
+++ kernel/src/framebuffer.rs
@@ -1,12 +1,8 @@
 #![allow(dead_code)]
 
+use core::char;
+
 use bootloader_api::info::{FrameBuffer, PixelFormat};
-use embedded_graphics::{
-    Pixel,
-    draw_target::DrawTarget,
-    geometry::{OriginDimensions, Size},
-    pixelcolor::{Rgb888, RgbColor},
-};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Position {
@@ -21,35 +17,10 @@ pub struct Color {
     pub blue: u8,
 }
 
-pub fn set_pixel(framebuffer: &mut FrameBuffer, position: Position, color: Color) {
-    let info = framebuffer.info();
-
-    let byte_offset = {
-        let y_offset = position.y * info.stride;
-        let pixel_offset = y_offset + position.x;
-
-        pixel_offset * info.bytes_per_pixel
-    };
-
-    let pixel_buffer = &mut framebuffer.buffer_mut()[byte_offset..];
-
-    match info.pixel_format {
-        PixelFormat::Rgb => {
-            pixel_buffer[0] = color.red;
-            pixel_buffer[1] = color.blue;
-            pixel_buffer[2] = color.green;
-        }
-        PixelFormat::Bgr => {
-            pixel_buffer[0] = color.blue;
-            pixel_buffer[1] = color.green;
-            pixel_buffer[2] = color.red;
-        }
-        PixelFormat::U8 => {
-            let gray = (color.red + color.green + color.blue) / 3;
-            pixel_buffer[0] = gray;
-        }
-        other => panic!("Unknown pixel format: {other:?}"),
-    }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Pixel {
+    pub position: Position,
+    pub color: Color,
 }
 
 pub(crate) struct FramebufferDisplay<'f> {
@@ -61,50 +32,36 @@ impl<'f> FramebufferDisplay<'f> {
         FramebufferDisplay { framebuffer }
     }
 
-    fn draw_pixel(&mut self, Pixel(coordinates, color): Pixel<Rgb888>) {
-        let (width, height) = {
-            let info = self.framebuffer.info();
-            (info.width, info.height)
-        };
-
-        let (x, y) = {
-            let c: (i32, i32) = coordinates.into();
-            (c.0 as usize, c.1 as usize)
-        };
-
-        if (0..width).contains(&x) && (0..height).contains(&y) {
-            let color = Color {
-                red: color.r(),
-                green: color.g(),
-                blue: color.b(),
-            };
-
-            set_pixel(self.framebuffer, Position { x, y }, color);
-        }
-    }
-}
-
-impl<'f> DrawTarget for FramebufferDisplay<'f> {
-    type Color = Rgb888;
-
-    type Error = core::convert::Infallible;
-
-    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
-    where
-        I: IntoIterator<Item = Pixel<Self::Color>>,
-    {
-        for pixel in pixels.into_iter() {
-            self.draw_pixel(pixel);
-        }
-
-        Ok(())
-    }
-}
-
-impl<'f> OriginDimensions for FramebufferDisplay<'f> {
-    fn size(&self) -> Size {
+    fn set_pixel(&mut self, position: Position, color: Color) {
         let info = self.framebuffer.info();
 
-        Size::new(info.width as u32, info.height as u32)
+        let byte_offset = {
+            let y_offset = position.y * info.stride;
+            let pixel_offset = y_offset + position.x;
+
+            pixel_offset * info.bytes_per_pixel
+        };
+
+        let pixel_buffer = &mut self.framebuffer.buffer_mut()[byte_offset..];
+
+        match info.pixel_format {
+            PixelFormat::Rgb => {
+                pixel_buffer[0] = color.red;
+                pixel_buffer[1] = color.blue;
+                pixel_buffer[2] = color.green;
+            }
+            PixelFormat::Bgr => {
+                pixel_buffer[0] = color.blue;
+                pixel_buffer[1] = color.green;
+                pixel_buffer[2] = color.red;
+            }
+            PixelFormat::U8 => {
+                let gray = (color.red + color.green + color.blue) / 3;
+                pixel_buffer[0] = gray;
+            }
+            other => panic!("Unknown pixel format: {other:?}"),
+        }
     }
+
+    fn draw_char(&mut self, char: char, pixel: Pixel) {}
 }
